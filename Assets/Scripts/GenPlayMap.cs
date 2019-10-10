a15baf0d7babdc9f9b16e740643441e2ca42013b
NaHCO3 <suqiuyu2019@foxmail.com>
2019-10-11 00:17:27 +0800

    nothing,摸了

---
+++ Assets/Scripts/GenPlayMap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenPlayMap : MonoBehaviour
+{
+	private GameObject[][] points;
+    private TransForm thisTransForm = null;
+
+    void Awake() {
+        thisTransForm = GetComponent<TransForm>();
+    }
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+	
+	public genMap(int m, int n) {
+		for (int i = 0;i < m;i++) {
+            for (int j = 0;j < n;j++) {
+                GameObject grid = Resources.Load("Textures/MapGrid") as GameObject;
+                grid.transform.position.x = i + 0.5;
+                grid.transform.position.y = n + 0.5;
+                grid.transform.parent = thisTransForm;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/MapGrid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGrid : MonoBehaviour
+{
+    private int x;
+    private int y;
+    private Transform thisTransform;
+    private SpriteRenderer thisSpriteRenderer;
+	// Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
