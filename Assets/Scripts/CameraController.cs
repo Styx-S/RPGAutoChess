7f5f59f5edff507f2b689ae0155c9ebaed85c72a
NaHCO3 <suqiuyu2019@foxmail.com>
2019-10-12 17:24:23 +0800

    feature:初始化摄像机位置

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Transform thisTransform = null;
+
+    private const string kControllerName = "GameController";
+
+    void Awake() {
+        thisTransform = GetComponent<Transform>();
+    }
+    // Start is called before the first frame update
+    void Start() {
+        initCamera();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void initCamera() {
+        MapGrid[][] grids = GameObject.FindGameObjectWithTag(kControllerName).GetComponent<MapManager>().mapGrids;
+        int m = grids.Length;
+        int n;
+        if (m > 0) {
+            n = grids[0].Length;
+        } else {
+            return;
+        }
+        Vector3 position = new Vector3(m / 2,n / 2,-10);
+        thisTransform.position = position;
+    }
+}
