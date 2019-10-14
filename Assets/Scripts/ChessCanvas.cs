48f0376ac3378b9fe88459983a89c5278e044050
NaHCO3 <suqiuyu2019@foxmail.com>
2019-10-14 20:22:43 +0800

    feature:完成Chess预制体

---
+++ Assets/Scripts/ChessCanvas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChessCanvas : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject chessText = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void displayText(string text) {
+        if (chessText != null) {
+            GameObject hud = Instantiate(chessText, transform)as GameObject;
+            hud.GetComponent<Text>().text = text;
+        }
+    }
+}
---
+++ Assets/Scripts/ChessText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ChessText : MonoBehaviour
+{
+    
+    private float speed = 1.5f;
+    private float timer = 0f;
+    private float time = 0.8f;
+    private Transform thisTransform = null;
+    
+    void Awake() {
+        thisTransform = GetComponent<Transform>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void scroll() {
+        thisTransform.Translate(Vector3.up * speed * Time.deltaTime);
+        timer += Time.deltaTime;
+        GetComponent<Text>().fontSize--;
+        GetComponent<Text>().color = new Color(1,0,0,1 - timer);
+        Destroy(gameObject,time);
+    }
+}
