67d0fc4f7bef6cdaf8566e581422b6b7503f32fa
StyxS <mail@styxs.cn>
2019-10-16 18:35:55 +0800

    feature: 添加主界面,添加游戏中返回主界面按钮

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject[] mainMenuElements;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void quitGame() {
+        Application.Quit();
+    }
+
+    public void alertAbout() {
+        
+    }
+
+    private bool visibility = true;
+    private void changeVisibility() {
+        visibility = !visibility;
+        foreach(GameObject gameObject in mainMenuElements) {
+            gameObject.SetActive(visibility);
+        }
+    }
+
+}
