2d66c39ca255d052bfcc86b3b2ce3bd2dd1d6392
NaHCO3 <suqiuyu2019@foxmail.com>
2019-11-10 23:18:08 +0800

    feature:实现RandomUtil

--- Assets/Scripts/Logic/Utils/RandomUtil.cs
+++ Assets/Scripts/Logic/Utils/RandomUtil.cs
@@ -1,2 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+
+public class RandomUtil {
+    private static RandomUtil instance = new RandomUtil();
+    private static Queue<int> randomQueue;
+    private RandomUtil() {
+        randomQueue = new Queue<int>();
+        init();
+    }
+
+    private void init () {
+        for (int i = 0;i < 10000;i++) {
+            System.Random r = new System.Random(System.Guid.NewGuid().GetHashCode());
+            randomQueue.Enqueue(r.Next());
+        } 
+    }
+
+    public static int next() {
+        int result = randomQueue.Dequeue();
+        randomQueue.Enqueue(result);
+        return result;
+    }
+}
